38f4c1a7d6bb5c87161e29976afdbc02e4fed6ce
Egor Gulyaev <egor.gulaev22@gmail.com>
2023-06-03 14:21:44 +0700

    june-third

--- js/button-up.js
+++ js/button-up.js
@@ -14,7 +14,7 @@ const btnUp = {
       // определяем величину прокрутки
       const scrollY = window.scrollY || document.documentElement.scrollTop;
       // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
-      scrollY > 2830 ? this.show() : this.hide();
+      scrollY > 1500 ? this.show() : this.hide();
     });
     // при нажатии на кнопку .btn-up
     document.querySelector('.btn-up').onclick = () => {
